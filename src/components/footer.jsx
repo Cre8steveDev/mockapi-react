99e9df739635cf7c2e9cfb1105e590e6668c64ab
Eguavoen Dunamis Duncan <duncandunamis@gmail.com>
2026-06-22 13:38:16 +0100

    Committing to hero Section Changes

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,11 +2,26 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <section>
-      <div className="main-footer-content">
-        <img src="/src/assets/image.png" alt="" className="logo-img" />
+    <footer>
+      <div className="footer-inner-cont">
+        <div className="main-footer-content">
+          <img src="/src/assets/image.png" alt="logo" className="logo-img" />
+          <p className="Business-Description">
+            Aptech Computer Education is a renowned ICT institute in Benin City,
+            Edo State
+          </p>
+        </div>
+        <div className="aside-content">
+          <h3>Navigation</h3>
+          <nav className="footer-nav-links">
+            <a href="index.html">Home</a>
+            <a href="about-us.html">About Us</a>
+            <a href="contact-us.html">Contact Us</a>
+            <a href="blog.html">Blog</a>
+          </nav>
+        </div>
       </div>
-    </section>
+    </footer>
   );
 };
 
