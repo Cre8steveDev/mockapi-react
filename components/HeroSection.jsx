5c81db4004eb6ef52a02be366246916044844644
Okolo Bright <okolobright49@gmail.com>
2026-06-22 13:23:11 +0100

    updated hero section

---
+++ components/HeroSection.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <section className="hero-section">
+      <div className="hero-body">
+        <div className="hero-writeup">
+          <h1>Aptech computer Education Benein City</h1>
+          <p>Premium IT institute in the heartbeat of the nation.</p>
+          <div className="hero-actions">
+            <button className="hero-button hero-button-primary">Get Started</button>
+            <button className="hero-button hero-button-secondary">Our Contacts</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default HeroSection
--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/footer";
+import HeroSection from "../components/HeroSection";
 
 const App = () => {
   return (
     <div>
       <section className="inner-container">
         <Header />
+        <HeroSection />
         <Footer />
       </section>
     </div>
